d776e9bc06f878e96772da8af86fe33f576d74cf
r.gasimov <ruslan.gasimov@artezio.com>
2018-10-25 15:28:32 +0400

    library-client-react-main: add_recompose - other

--- library-client-react-main/src/components/AuthorForm/index.js
+++ library-client-react-main/src/components/AuthorForm/index.js
@@ -5,13 +5,11 @@ import withForm from "../../hocs/withForm";
 import {saveOrUpdateAuthor} from "../../api/authorsApi";
 
 const withHandleForm = compose(
-    withHandlers(
-        {
-            onSubmit: props => (data, okFn, errFn) => {
-                saveOrUpdateAuthor(data, okFn, errFn);
-            }
+    withHandlers({
+        onSubmit: props => (data, okFn, errFn) => {
+            saveOrUpdateAuthor(data, okFn, errFn);
         }
-    ),
+    }),
     withForm
 );
 export default compose(withHandleForm)(AuthorForm);
