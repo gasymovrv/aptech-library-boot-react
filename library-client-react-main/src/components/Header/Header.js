fbf873f715df3133fe2a5a9512ddd8d40444bf46
r.gasimov <ruslan.gasimov@artezio.com>
2018-10-29 13:07:14 +0400

    library-client-react-main: add_router - ссылки в Header и рефакторинг его

--- library-client-react-main/src/components/Header/Header.js
+++ library-client-react-main/src/components/Header/Header.js
@@ -1,64 +1,37 @@
 import React from 'react';
 import Watch from "../Watch";
 import logo from '../../img/BeSmart-logo.svg' // relative path to image
+import {Link} from "react-router-dom";
 
-export default class Header extends React.Component {
-
-    state = {
-        isActiveWatch: false,
-        watchText: "Включить часы"
-    };
-
-    toggleIsActiveWatch = () => {
-        if (!this.state.isActiveWatch) {
-            this.setState({
-                isActiveWatch: true,
-                watchText: "Выключить часы"
-            })
-        } else {
-            this.setState({
-                isActiveWatch: false,
-                watchText: "Включить часы"
-            })
-        }
-    };
-
-    render() {
-        const {watchText, isActiveWatch} = this.state;
-        return (
-            <div className="mainmenu-wrapper">
-                <div className="container">
-                    <div className="row">
-                        <ul className="list-inline">
-                            <li><input className="btn btn-sm" type="button" onClick={this.toggleIsActiveWatch} value={watchText}/></li>
-                            <li><Watch isActive={isActiveWatch}/></li>
-                        </ul>
-                    </div>
-                    <div className="menuextras">
-                        <div className="extras">
-                            <ul>
-                                <li>
-                                    <i className="glyphicon glyphicon-user icon-white"/> <a href="#">Авторизация</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <nav id="mainmenu" className="mainmenu">
+export default function Header({watchText, isActiveWatch, onToggleWatch}) {
+    return (
+        <div className="mainmenu-wrapper">
+            <div className="container">
+                <Watch isActive={isActiveWatch} watchText={watchText} onToggleWatch={onToggleWatch}/>
+                <div className="menuextras">
+                    <div className="extras">
                         <ul>
-                            <li className="logo-wrapper"><a href="#"><img src={logo} alt="Изображение не найдено"/></a></li>
                             <li>
-                                <a href="/">Все книги</a>
-                            </li>
-                            <li>
-                                <a href="/authors">Авторы</a>
-                            </li>
-                            <li>
-                                <a href="#">О нас</a>
+                                <i className="glyphicon glyphicon-user icon-white"/> <a href="#">Авторизация</a>
                             </li>
                         </ul>
-                    </nav>
+                    </div>
                 </div>
+                <nav id="mainmenu" className="mainmenu">
+                    <ul>
+                        <li className="logo-wrapper"><a href="#"><img src={logo} alt="Изображение не найдено"/></a></li>
+                        <li>
+                            <Link to="/">Все книги</Link>
+                        </li>
+                        <li>
+                            <Link to="/authors">Авторы</Link>
+                        </li>
+                        <li>
+                            <Link to="#">О нас</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-        )
-    }
+        </div>
+    )
 }
